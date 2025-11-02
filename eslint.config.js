0aa740b78bbd7cb7a38e9995bde25472c7cd8870
Le Ba Hau <haubtm699@gmail.com>
2025-11-02 14:32:22 +0700

    chore: update dependencies and add Tailwind CSS support
    - Updated various dependencies in package.json, including Expo and React Native.
    - Added Prettier configuration for code formatting.
    - Introduced PostCSS configuration for Tailwind CSS.
    - Created a Tailwind CSS configuration file.
    - Modified explore.tsx to utilize NativeWind for styling.
    - Updated _layout.tsx to include global CSS.
    - Adjusted tsconfig.json to include nativewind-env.d.ts for TypeScript support.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
-// https://docs.expo.dev/guides/using-eslint/
+import prettierPlugin from 'eslint-plugin-prettier';
+
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
@@ -8,11 +9,15 @@ module.exports = defineConfig([
     ignores: ['dist/*'],
   },
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       parserOptions: {
         project: './tsconfig.json',
       },
+    },
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': 'error'
     },
     settings: {
       'import/resolver': {
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // bao trùm mọi file có dùng className
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
